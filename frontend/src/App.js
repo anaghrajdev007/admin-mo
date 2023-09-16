78540b0919101d2c0feeeaf89e80c55c65f0ad72
Anagh Raj <unknowndev0007@gmail.com>
2023-09-16 13:42:36 +0530

    Navbar Finished
    NAvbar Finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import {useState} from "react"
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
   const[sidebarOpne, setSidebarOpen] = useState(false);
 
@@ -11,6 +12,7 @@ const App = () => {
   }
   return (
     <div className = "container">
+      <Navbar sidebarOpen={sidebarOpne} openSidebar={openSidebar}/>
       <h1>Dashboard</h1>
     </div>
   );
--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -7,12 +7,23 @@ const Navbar = ({sidebarOpen, openSidebar}) => {
   return (
     <nav className="navbar">
     <div className="nav_icon" onClick={()=>openSidebar()}>
-        <i className="fa fa-bars"></i>
+    <i class='bx bx-menu'></i>
     </div>
     <div className="navbar__left">
-        <a href="#">Option</a>
-        <a href="#">option</a>
-        <a href="#"></a>
+        <a href="#">Subscrivers</a>
+        <a href="#">Video MAnagers</a>
+        <a href="#">Admin</a>
+    </div>
+    <div className="navbar__right">
+      <a href="">
+      <i class='bx bx-search-alt'></i>
+      </a>
+      <a href="#">
+      <i class='bx bx-stopwatch'></i>
+      </a>
+      <a href="#">
+        <img width="30" src={image} alt="avatar" />
+      </a>
     </div>
 
     </nav>
